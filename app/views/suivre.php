2c4e38eaf938805dcb20e8f56d1aa9cab2cb2925
RAYID DJERI <58982423+dR-Corp@users.noreply.github.com>
2024-04-30 09:36:36 +0100

    Gestion de la page de suivi

--- app/views/suivre.php
+++ app/views/suivre.php
@@ -19,6 +19,36 @@
 
         <div class="row">
             
+            <div class="col-md-12">
+
+                <div class="callout callout-info">
+                    <h5><i class="fas fa-info"></i> Note:</h5>
+                    Saisisser le numéro de référence de la demande, puis tapez sur vérifier pour savoir le statut de votre demande de réservation.
+                </div>
+                
+                <div class="card card-primary card-outline">
+                    
+                    <form role="form">
+                        <div class="card-body">
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1 text-lg">Numéro de référence</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="">
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Vérifier</button>
+                            </div>
+                        
+                        </div>
+
+                    </form>
+                </div>
+
+            </div>
+                
+            </div>
+
         </div>
         
     </div>
